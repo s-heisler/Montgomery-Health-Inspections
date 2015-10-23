b0b52e5e6d9bcf3359536e936c84f48d09f35493
sversh <sofia@mainstreetgenome.com>
2015-10-23 14:26:54 -0400

    Health violation and crime data processing

---
+++ Scripts/01 load health inspections data.R
@@ -0,0 +1,70 @@
+geneorama::loadinstall_libraries("data.table")
+require("plyr")
+require("data.table")
+
+## Import shift function
+shift <- geneorama::shift
+
+##set working directory
+setwd('/Users/sofiaver/Dropbox (Personal)/Odd jobs/Health inspections')
+
+#load in the data
+foodInspect <- read.csv("Moco data/Food_Inspection.csv")
+
+##fix variable names
+foodInspect<-setNames(foodInspect, gsub("\\.","_",colnames(foodInspect)))
+foodInspect<-setNames(foodInspect, gsub("_+$","",colnames(foodInspect)))
+
+#Convert dates to date
+foodInspect$Inspection_Date <- as.Date(foodInspect$Inspection_Date, "%m/%d/%Y")
+
+#Check that there are no missing dates
+sum(is.na(foodInspect$Inspection_Date))
+
+#Remove periods (most common cause of inconsistency) from address
+foodInspect$addr <- gsub('[.]', '', foodInspect$Address_1)
+
+#Remove other messy characters from names
+foodInspect$Name <- gsub('-', ' ', foodInspect$Name)
+foodInspect$Name <- gsub('\'', '', foodInspect$Name)
+foodInspect$Name <- gsub('&', 'AND', foodInspect$Name)
+
+#Create a unique restaurant id
+foodInspect$id <- id(foodInspect[c("Name", "addr")], drop = FALSE)
+
+#Fill in and standardize missing coordinates when available
+foodInspect <- data.table(foodInspect)
+setkey(foodInspect, id)
+foodInspect$Latitude[is.na(foodInspect$Latitude)] <- 0
+foodInspect$Longitude[is.na(foodInspect$Longitude)] <- 0
+coords_agg <- foodInspect[,list(stdLat=max(Latitude), stdLon=min(Longitude)),by=id]
+foodInspect <- join(foodInspect, coords_agg, by='id')
+
+#Drop rows with missing coordinates
+foodInspect <- foodInspect[foodInspect$stdLat!=0]
+
+#Calculate whether the restaurant failed the inspection
+foodInspect[ , fail_flag := ifelse(Inspection_Results %in% c("Critical Violations Corrected", "Facility Closed"),1, 0)]
+
+#Sort table by id and date of inspection
+foodInspect <- foodInspect[order(id, Inspection_Date)]
+
+#Calculate failure on previous inspection
+foodInspect[ , past_fail := shift(fail_flag, -1, 0), by = id]
+
+#Calculate time since last inspection
+foodInspect[, last_insp := c(0, Inspection_Date[-.N]) ,by=id]
+foodInspect$days_since_insp = foodInspect$Inspection_Date-as.Date(foodInspect$last_insp, '1970-01-01')
+foodInspect$days_since_insp[foodInspect$last_insp==0] <- NA
+
+
+#Export table to csv for review
+write.table(foodInspect, "Temp/Food inspect full processed data.csv", quote = FALSE, sep = "|", row.names = FALSE)
+
+#Keep only the important variables
+foodInspectShort <- foodInspect[, list(id, Name, addr, City, Zip, Category, Type, stdLat, stdLon, Inspection_Date, fail_flag, past_fail, days_since_insp)]
+
+#Save
+saveRDS(foodInspect , "Data/food_inspections.Rds")
+
+
---
+++ Scripts/02 load crime data.R
@@ -0,0 +1,38 @@
+require("data.table")
+
+#load, in the data
+crime <- read.csv("Moco data/Moco crime data.csv")
+
+##fix variable names
+crime<-setNames(crime, gsub("\\.","_",colnames(crime)))
+crime<-setNames(crime, gsub("___","_",colnames(crime)))
+
+#Adjust date formats
+crime$crime_date <- as.Date(crime$Start_Date_Time, "%m/%d/%Y %I:%M:%S %p")
+sum(is.na(crime$crime_date))
+
+#Convert into data.table
+crime <- data.table(crime)
+
+#Limit to relevant data range
+crime <- crime[crime_date >= as.IDate("2013-01-01")]
+
+#Create flags for different crime types
+crime[ , crime_burglary_flag := ifelse(Class_Description %like% 'BURG ',1, 0)]
+crime[ , crime_larceny_flag := ifelse((Class_Description %like% 'LARCENY') | (Class_Description %like% 'ROB '),1, 0)]
+crime[ , crime_vandalism_flag := ifelse(Class_Description %like% 'VANDALISM ',1, 0)]
+crime[ , crime_drug_flag := ifelse(Class_Description %like% 'CDS ',1, 0)]
+crime[ , crime_any_flag := ifelse((crime_burglary_flag==1) | (crime_larceny_flag==1) | (crime_vandalism_flag==1) | (crime_drug_flag==1) ,1, 0)]
+
+#Keep only relevant crime types
+crime <- crime[crime$crime_any_flag==1]
+
+#Drop everything with missing lat/lon
+print(cat('Fraction of rows missing coords:', sum(is.na(crime$Latitude))/NROW(crime)))
+crime <- crime[!is.na(crime$Latitude)]
+
+#Save data
+write.table(crime, "Temp/Crime full processed data.csv", quote = FALSE, sep = "|", row.names = FALSE)
+crime_short <- crime[ , list(crime_date, crime_burglary_flag, crime_larceny_flag, crime_vandalism_flag, crime_drug_flag, Latitude, Longitude)]
+
+saveRDS(crime_short, 'Data/Crime.Rds')
