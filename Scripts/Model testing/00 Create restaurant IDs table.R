2199402e50521efcf3b1eabc6aa7b242147da685
sversh <sofia@mainstreetgenome.com>
2015-11-11 21:52:45 -0500

    Fix bugs and add model testing

--- Scripts/Model testing/00 Create restaurant IDs table.R
+++
@@ -1,8 +0,0 @@
-#Create a unique list of establishment IDs
-foodId <- readRDS("Data/food_inspections.Rds")
-
-foodId <- foodId[, list(Name, addr, City, Zip, id)]
-foodId <- unique(foodId)
-
-saveRDS(foodId , "Data/Restaurant IDs in food inspections.Rds")
-
